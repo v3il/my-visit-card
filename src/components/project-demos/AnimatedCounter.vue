30aa942140f1ab633e6e9cc7e99495404be2a4e8
Dima <v3il@ukr.net>
2018-09-11 21:15:17 +0300

    Animated counter demo page

--- src/components/project-demos/AnimatedCounter.vue
+++ src/components/project-demos/AnimatedCounter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="demo-page">
         <div class="counter counter-simple">
             <div class="animated-counter-element js-animated-counter-element-simple">
                 {{simple.initial}}
@@ -8,26 +8,40 @@
             <div class="counter-params">
                 <label>
                     Animate to:
-                    <input type="text" v-model="simple.to">
+                    <input type="number" v-model="simple.to">
                 </label>
 
                 <label>
                     Duration:
-                    <input type="text" v-model="simple.duration">
+                    <input type="number" v-model="simple.duration">
                 </label>
 
                 <label>
                     Delay:
-                    <input type="text" v-model="simple.delay">
+                    <input type="number" v-model="simple.delay">
                 </label>
 
                 <label>
                     Result precision:
-                    <input type="text" v-model="simple.precision">
+                    <input type="number" v-model="simple.precision">
                 </label>
 
-                <button @click="animateSimple()">Animate</button>
-                <button @click="setRandom('simple')">Set random</button>
+                <div class="counter-actions">
+                    <button @click="animateSimple()">Animate</button>
+                    <button @click="setRandom('simple')">Set random</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="counter counter-colorful">
+            <div class="animated-counter-element js-animated-counter-element-colorful">
+                {{simple.initial}}
+            </div>
+
+            <div class="counter-params">
+                <div class="counter-actions">
+                    <button @click="animateColorful()">Animate</button>
+                </div>
             </div>
         </div>
     </div>
@@ -47,6 +61,14 @@
                     to: 0,
                     duration: 1000,
                     precision: 0,
+                },
+
+                colorful: {
+                    initial: 0,
+                    delay: 0,
+                    to: 1000,
+                    duration: 1000,
+                    precision: 0,
                 }
             }
         },
@@ -59,6 +81,28 @@
                     delay: counterParams.delay,
                     animationTime: counterParams.duration,
                     resultPrecision: counterParams.precision,
+                });
+            },
+
+            animateColorful() {
+                const counterParams = this.colorful;
+
+                counterParams.instance.setValue(1000, {
+                    delay: 0,
+                    animationTime: 3000,
+                    resultPrecision: 0,
+                });
+
+                counterParams.instance.on('animated-counter-tick', (currentValue) => {
+                    counterParams.element.classList.remove('yellow', 'red');
+
+                    if (currentValue > 400 && currentValue < 800) {
+                        counterParams.element.classList.add('yellow');
+                    }
+
+                    if (currentValue >= 800) {
+                        counterParams.element.classList.add('red');
+                    }
                 });
             },
 
@@ -83,19 +127,95 @@
         },
 
         mounted() {
-            ['simple']
+            ['simple', 'colorful']
                 .forEach((counterName) => {
-                    this[counterName].instance = new AnimatedCounter(
-                        this._getCounterElementByClass(counterName)
-                    );
+                    const counterElement = this._getCounterElementByClass(counterName);
+
+                    this[counterName].element = counterElement;
+                    this[counterName].instance = new AnimatedCounter(counterElement);
                 });
         }
     }
 </script>
 
 <style scoped>
-    /*div {*/
-        /*height: 100vh;*/
-        /*background-color: #fff;*/
-    /*}*/
+    .demo-page {
+        max-width: 1200px;
+        width: 100%;
+        margin: 0 auto;
+    }
+
+    .counter {
+        display: flex;
+        padding: 24px;
+    }
+
+    @media screen and (max-width: 600px) {
+        .counter {
+            flex-direction: column;
+        }
+
+        .animated-counter-element {
+            padding: 18px 0;
+        }
+    }
+
+    .animated-counter-element, .counter-params {
+        flex-basis: 50%;
+    }
+
+    .animated-counter-element {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-size: 36px;
+    }
+
+    .animated-counter-element.yellow {
+        color: darkorange;
+    }
+
+    .animated-counter-element.red {
+        color: lightcoral;
+    }
+
+    .animated-counter-element {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        font-size: 36px;
+    }
+
+    .counter-params {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .counter-actions {
+        margin-top: 12px;
+    }
+
+    input {
+        width: 100%;
+        padding: 6px 12px;
+        border-radius: 3px;
+        outline: none;
+        border: 1px solid #4c4c4c;
+    }
+
+    label {
+        font-weight: bold;
+    }
+
+    button {
+        padding: 6px 12px;
+        background: #3971ff;
+        color: white;
+        border: 1px solid #3971ff;
+        outline: none;
+        border-radius: 3px;
+        cursor: pointer;
+    }
 </style>
