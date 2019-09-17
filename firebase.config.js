2b1c810675cd30f703b92642cf0cd51f840e7e91
Dima <v3il@ukr.net>
2019-09-17 19:58:20 +0300

    Removed firebase, design updates

--- firebase.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    apiKey: "AIzaSyBG27M3vVKYRWvgWqDymIldbcQ217rGf2g",
-    authDomain: "veil-vc.firebaseapp.com",
-    databaseURL: "https://veil-vc.firebaseio.com",
-    projectId: "veil-vc",
-    storageBucket: "veil-vc.appspot.com",
-    messagingSenderId: "1073981351789",
-}
